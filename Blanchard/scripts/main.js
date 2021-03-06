142aecc935caf85b13f3ce2073fafa4d120cd7fd
Roman Antipov <korona3001@gmail.com>
2021-03-06 16:00:56 +0300

    validate and mask plugin added

--- Blanchard/scripts/main.js
+++ Blanchard/scripts/main.js
@@ -278,16 +278,38 @@ function init(){
       center: [55.758332997776776,37.60265127448263],
       zoom: 15
   });
-
   var myPlacemark = new ymaps.Placemark([55.758332997776776,37.60265127448263], {}, {
     iconLayout: 'default#image',
     iconImageHref: './img/icon-formaps.svg',
     iconImageSize: [20, 20],
     iconImageOffset: [-3, -42]
   });
-
   myMap.geoObjects.add(myPlacemark);
 }
+
+let selector = document.querySelector("input[type='tel']");
+let im = new Inputmask("+7 (999)-999-99-99");
+im.mask(selector);
+
+new JustValidate('.js-contacts-form', {
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+    },
+    tel: {
+      required: true,
+      function: (name, value) => {
+        const phone = selector.inputmask.unmaskedvalue()
+        return Number(phone) && phone.length === 10
+      }
+    },
+    email: {
+      required: false,
+      email: true
+    },
+  },
+});
 
 // $('a').click(function(e) {
 //   e.preventDefault();
